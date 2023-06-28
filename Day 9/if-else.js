b82bf0da3c0116bbd594622c19d42e23995593cc
Jeel0077 <137033456+Jeel0077@users.noreply.github.com>
2023-06-28 15:27:45 +0530

    Add files via upload

---
+++ Day 9/if-else.js
@@ -0,0 +1,9 @@
+var num = 10;
+
+if (num > 10) {
+  console.log("The number is greater than 10");
+} else if (num < 10) {
+  console.log("The number is less than 10");
+} else {
+  console.log("The number is equal to 10");
+}
---
+++ Day 9/switch-case.js
@@ -0,0 +1,15 @@
+var fruit = "apple";
+
+switch (fruit) {
+  case "apple":
+    console.log("Selected fruit is an apple");
+    break;
+  case "banana":
+    console.log("Selected fruit is a banana");
+    break;
+  case "orange":
+    console.log("Selected fruit is an orange");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
